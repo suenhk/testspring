93082d277d43d64bba35858017702500e24963c9
Suen <shk8000@163.com>
2016-08-09 17:52:43 +0800

    Update Testone.java

--- src/main/java/com/shk8000/test/Testone.java
+++ src/main/java/com/shk8000/test/Testone.java
@@ -1,6 +1,6 @@
 public class Testone{
 	public static void main(String[] args){
 		System.out.println("GGGGGGGGGGGGGGGGGGG");
-		System.out.println("ttttGGGGGGGGGGGGGGGGGGGoooooooooooooooo");
+		System.out.println("ttttGGGGGGGGGGGGGGGGGGGoo111oooooooooooooo");
 	}
 }
